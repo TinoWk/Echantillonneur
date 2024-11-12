5f7b17e84c37c2538a85b65755499be598038243
TinoWk <142443557+TinoWk@users.noreply.github.com>
2024-11-12 10:51:14 +0000

    Update Echantillonneur(Analogie_a_numerique).py

--- Echantillonneur(Analogie_a_numerique).py
+++ Echantillonneur(Analogie_a_numerique).py
@@ -1,2 +1,21 @@
-# ce programme ve impementer la méthode de rectange, de trapèze pour passer de signal continue à un signal nulérique (UE_SSS(COURS D'AUTOMATIQUE))
-# CODAGE EN COURS...
+# Ce programme va impementer la méthode de rectangle, de trapèze pour passer de signal continue à un signal numérique (UE_SSS(COURS D'AUTOMATIQUE))
+# nous allons implementer les differentes méthodes pour la réalisation du projet
+import numpy
+# fonction de convertion d'expression
+def convertir_fonction(expression):
+    return lambda t:eval(expression)
+fonction=convertir_fonction("2*t")
+
+# fonction calcule surface
+def calcul_surface(temp_initial,temp_final):
+    return fonction(temp_initial)*(temp_final-temp_initial)
+
+#boucle pour parcourir tous les triangles
+def Echantillon(temps_initial,temps_final,periode):
+    stockage=0.0
+    for i in range(int((temps_final-temps_initial)/periode)):
+        stockage+= calcul_surface(temps_initial,temps_initial+periode)
+        temps_initial+=periode  
+    return stockage
+
+print(Echantillon(0,20,2))
